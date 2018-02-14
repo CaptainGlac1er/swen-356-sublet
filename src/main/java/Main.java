db774ed555bde15dc90c4c2351ea4ae42ec027a7
George Colgrove <georgecolgrove@outlook.com>
2018-02-14 12:12:18 -0500

    added velocity

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,21 @@
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import static spark.Spark.*;
 
 public class Main {
     public static void main(String[] args) {
+        get("/", (req,res)->{
+            Map<String,Object> model = new HashMap<>();
+            model.put("message","Hello World 123");
+            return new ModelAndView(model, "velocity/index.vm");
+        }, new VelocityTemplateEngine());
         get("/hello", (req, res) -> "Hello World");
+        get("/hello/:name", (req,res)->{
+            return "Hello" + req.params("name");
+        });
     }
 }
