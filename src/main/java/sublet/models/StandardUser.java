9513b82432cb983706c5cedd0d16ba4f5aece5fe
Benjamin Hunt <bjh1381@rit.edu>
2018-02-18 16:45:10 -0500

    added basic model for user

---
+++ src/main/java/sublet/models/StandardUser.java
@@ -0,0 +1,29 @@
+package sublet.models;
+import java.util.Date;
+
+public class StandardUser {
+    private String UID;
+    private String fname;
+    private String lname;
+    private String username;
+    private String password;
+    private String email;
+    private Date birthday;
+    private Date gradYear;
+
+    public StandardUser(String UID, String fname, String lname, String username, String pass, String email, Date birthday, Date gradYear){
+        this.UID = UID;
+        this.fname = fname;
+        this.lname = lname;
+        this.username = username;
+        this.password = pass;
+        this.email = email;
+        this.birthday = birthday;
+        this.gradYear = gradYear;
+    }
+
+    public boolean checkPass(String UID, String pw){
+        return UID.equals(pw);
+    }
+
+}
