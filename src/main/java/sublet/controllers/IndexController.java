48e7f39a6622ee8c00ad30de58efe6a440fd816d
Ethan Della Posta <ejd3081@rit.edu>
2018-02-18 15:59:36 -0500

    merge conflict

--- src/main/java/sublet/controllers/IndexController.java
+++ src/main/java/sublet/controllers/IndexController.java
@@ -17,7 +17,7 @@ public class IndexController {
 
         String desc = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse luctus augue nec sollicitudin aliquam. Maecenas id viverra velit. Nam molestie finibus urna a iaculis. Sed non venenatis urna. Vestibulum vestibulum enim justo, quis dictum mauris mollis quis. Quisque malesuada nulla quis mollis mollis. Vivamus sed feugiat neque. Fusce vel leo vitae est laoreet venenatis. ";
 
-        Listing listing = new Listing("user123@rit.edu", desc, "$500/month");
+        Listing listing = new Listing("user123@rit.edu", desc, "$500/month", "male");
         ArrayList<Listing> listings = new ArrayList<>();
         listings.add((listing));
         listings.add((listing));
