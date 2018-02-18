63d852af63d34905e9bf5ea50f6102e927117aa0
CaptainGlac1er <georgecolgrove@outlook.com>
2018-02-18 15:27:36 -0500

    updated index page

--- src/main/java/sublet/controllers/IndexController.java
+++ src/main/java/sublet/controllers/IndexController.java
@@ -7,6 +7,7 @@ import sublet.models.Listing;
 import sublet.util.PageRender;
 import sublet.util.Path;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,8 +18,13 @@ public class IndexController {
         String desc = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse luctus augue nec sollicitudin aliquam. Maecenas id viverra velit. Nam molestie finibus urna a iaculis. Sed non venenatis urna. Vestibulum vestibulum enim justo, quis dictum mauris mollis quis. Quisque malesuada nulla quis mollis mollis. Vivamus sed feugiat neque. Fusce vel leo vitae est laoreet venenatis. ";
 
         Listing listing = new Listing("user123@rit.edu", desc, "$500/month");
+        ArrayList<Listing> listings = new ArrayList<>();
+        listings.add((listing));
+        listings.add((listing));
+        listings.add((listing));
+        listings.add((listing));
 
-        model.put("listing",listing);
+        model.put("listings",listings);
         return PageRender.render(request,model, Path.Template.INDEX);
     };
 }
