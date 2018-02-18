ee79fb272a161f097adfa466ace9199f145f0577
MEF4232 <mollyelizfeld@gmail.com>
2018-02-18 17:01:17 -0500

    Updated Listing fields with public enum

--- src/main/java/sublet/models/Listing.java
+++ src/main/java/sublet/models/Listing.java
@@ -1,13 +1,15 @@
 package sublet.models;
 
-import java.util.List;
-import static java.util.Arrays.asList;
+// import java.util.List;
+// import static java.util.Arrays.asList;
 
 public class Listing {
 
     private String user;
     private String desc;
     private String rent;
+    public enum PaymentFrequencyOptions{ MONTHLY, SEMESTERLY }
+    PaymentFrequencyOptions payment_frequency;
     public enum GenderOptions{ COED, FEMALE, MALE }
     GenderOptions gender;
     public enum HousingTypeOptions{ COLONYMANOR, GLOBALVILLAGE, PARKPOINT, PERKINSGREEN, PROVINCE, RACQUETCLUB, RITINN,
@@ -15,6 +17,7 @@ public class Listing {
     HousingTypeOptions housing_type;
     public enum IsFurnishedOptions{ FURNISHED, UNFURNISHED, PARTIALLYFURNISHED }
     IsFurnishedOptions is_furnished;
+
     /* private List<String> pick_gender = asList("Coed", "Female only", "Male only");
     private String gender;
     private List<String> pick_housing_type = asList("Colony Manor", "Global Village", "Park Point", "Perkins Green",
@@ -26,11 +29,17 @@ public class Listing {
     */
 
 
-    public Listing(String user, String desc, String rent, GenderOptions gender, HousingTypeOptions housing_type,
+    public Listing(String user,
+                   String desc,
+                   String rent,
+                   PaymentFrequencyOptions payment_frequency,
+                   GenderOptions gender,
+                   HousingTypeOptions housing_type,
                    IsFurnishedOptions is_furnished){
         this.user = user;
         this.desc = desc;
         this.rent = rent;
+        this.payment_frequency = payment_frequency;
         this.gender = gender;
         this.housing_type = housing_type;
         this.is_furnished = is_furnished;
@@ -44,6 +53,9 @@ public class Listing {
     }
     public String getRent(){
         return this.rent;
+    }
+    public PaymentFrequencyOptions getPayment_frequency() {
+        return this.payment_frequency;
     }
     public GenderOptions getGender(){
         return this.gender;
