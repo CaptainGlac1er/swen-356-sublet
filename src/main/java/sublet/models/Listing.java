6c272024e012551ebce8c8d99c5d535fbf986db3
MEF4232 <mollyelizfeld@gmail.com>
2018-02-18 15:28:32 -0500

    Updated Listing field options

--- src/main/java/sublet/models/Listing.java
+++ src/main/java/sublet/models/Listing.java
@@ -1,14 +1,30 @@
 package sublet.models;
+// gender, rit housing, furnished
+
+import java.util.List;
+import static java.util.Arrays.asList;
 
 public class Listing {
     private String user;
     private String desc;
     private String rent;
+    private List<String> pick_gender = asList("Coed", "Female only", "Male only");
+    private String gender;
+    private List<String> pick_housing_type = asList("Colony Manor", "Global Village", "Park Point", "Perkins Green",
+            "Province", "Racquet Club", "RIT Inn & Conference Center", "Riverknoll", "The Lodge", "University Commons",
+            "Off campus");
+    private String housing_type;
+    private List<String> pick_is_furnished = asList("Furnished", "Unfurnished", "Partially furnished");
+    private String is_furnished;
 
-    public Listing(String user, String desc, String rent){
+
+    public Listing(String user, String desc, String rent, String gender, String housing_type, String is_furnished){
         this.user = user;
         this.desc = desc;
         this.rent = rent;
+        this.gender = gender;
+        this.housing_type = housing_type;
+        this.is_furnished = is_furnished;
     }
 
     public String getUser(){
@@ -19,5 +35,14 @@ public class Listing {
     }
     public String getRent(){
         return this.rent;
+    }
+    public String getGender(){
+        return this.gender;
+    }
+    public String getHousingType(){
+        return this.housing_type;
+    }
+    public String getIsFurnished(){
+        return this.is_furnished;
     }
 }
