7e524935bb8be21302b6be3aaa525e0e463f297a
sjc5897 <sjc5897@g.rit.edu>
2018-02-28 18:42:05 -0500

    Forgot to eliminate the print statements I used to debug

--- src/main/java/sublet/Commands/AddUser.java
+++ src/main/java/sublet/Commands/AddUser.java
@@ -12,9 +12,7 @@ public class AddUser implements Command {
     @Override
     public void Execute(Controller controller) {
         Request request = controller.getCurrentRequest();
-        System.out.println("A");
         if (!controller.isLoggedIn()) {
-            System.out.println("B");
             StandardUser user = new StandardUser(new Random().nextLong(),
                     request.queryParams("fname"),
                     request.queryParams("lname"),
@@ -22,13 +20,8 @@ public class AddUser implements Command {
                     request.queryParams("password"),
                     request.queryParams("email"),
                     new Date(12345), new Date(3456));
-
             CurrentUser.registerUser(user);
             controller.addRedirect(Path.Web.USER);
-            System.out.println(user.getUsername());
-            System.out.println(user.getPassword());
-
-
         }
     }
 }
