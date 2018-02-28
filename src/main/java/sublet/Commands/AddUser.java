96ac4a39e6137e5fad497e50d2eed3d0a6faa442
sjc5897 <sjc5897@g.rit.edu>
2018-02-28 18:38:17 -0500

    Fixed the login bug
    I, stephen, did it mostly they watched. Called new listing for some reason. They also put the info in the constructor wrong. Those goofs
    
    Co-Authored-By: Benjamin Hunt <benjaminhunt@users.noreply.github.com>
    Co-Authored-By: gorosin <gorosin@users.noreply.github.com>

--- src/main/java/sublet/Commands/AddUser.java
+++ src/main/java/sublet/Commands/AddUser.java
@@ -12,16 +12,23 @@ public class AddUser implements Command {
     @Override
     public void Execute(Controller controller) {
         Request request = controller.getCurrentRequest();
+        System.out.println("A");
         if (!controller.isLoggedIn()) {
+            System.out.println("B");
             StandardUser user = new StandardUser(new Random().nextLong(),
                     request.queryParams("fname"),
                     request.queryParams("lname"),
                     request.queryParams("username"),
-                    request.queryParams("email"),
                     request.queryParams("password"),
+                    request.queryParams("email"),
                     new Date(12345), new Date(3456));
-            controller.addRedirect(Path.Web.USER);
+
             CurrentUser.registerUser(user);
+            controller.addRedirect(Path.Web.USER);
+            System.out.println(user.getUsername());
+            System.out.println(user.getPassword());
+
+
         }
     }
 }
