6bc7342fe0c05332c3e627957db81fe6b29eda62
CaptainGlac1er <georgecolgrove@outlook.com>
2018-03-10 18:00:55 -0500

    quickfix so the database doesn't explode with test listings

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,6 @@ import sublet.service.UserService;
 import java.io.InputStream;
 import java.time.LocalDate;
 import java.util.Properties;
-import java.util.Random;
 
 import static spark.Spark.*;
 
@@ -51,23 +50,23 @@ public class Main {
 
         String desc = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse luctus augue nec sollicitudin aliquam. Maecenas id viverra velit. Nam molestie finibus urna a iaculis. Sed non venenatis urna. Vestibulum vestibulum enim justo, quis dictum mauris mollis quis. Quisque malesuada nulla quis mollis mollis. Vivamus sed feugiat neque. Fusce vel leo vitae est laoreet venenatis. ";
 
-        Listing listing = new Listing((new Random()).nextLong(), Users.getCurrentUserUID(1234), desc, "500",
+        Listing listing = new Listing(1, Users.getCurrentUserUID(1234), desc, "500",
                 "55 Ocean Street",Listing.PaymentFrequencyOptions.MONTHLY, Listing.GenderOptions.MALE,
                 Listing.HousingTypeOptions.PARKPOINT, Listing.IsFurnishedOptions.FURNISHED, Listing.ParkingOption.ON_STR, false);
         Listings.AddListing(listing);
-        listing = new Listing((new Random()).nextLong(), Users.getCurrentUserUID(12345), desc, "500",
+        listing = new Listing(2, Users.getCurrentUserUID(12345), desc, "500",
                 "55 Ocean Street",Listing.PaymentFrequencyOptions.MONTHLY, Listing.GenderOptions.MALE,
                 Listing.HousingTypeOptions.PARKPOINT, Listing.IsFurnishedOptions.FURNISHED, Listing.ParkingOption.ON_STR, true);
         Listings.AddListing(listing);
-        listing = new Listing((new Random()).nextLong(), Users.getCurrentUserUID(1234), desc, "500",
+        listing = new Listing(3, Users.getCurrentUserUID(1234), desc, "500",
                 "55 Ocean Street",Listing.PaymentFrequencyOptions.MONTHLY, Listing.GenderOptions.MALE,
                 Listing.HousingTypeOptions.PARKPOINT, Listing.IsFurnishedOptions.FURNISHED, Listing.ParkingOption.ON_STR, false);
         Listings.AddListing(listing);
-        listing = new Listing((new Random()).nextLong(), Users.getCurrentUserUID(12345), desc, "500",
+        listing = new Listing(4, Users.getCurrentUserUID(12345), desc, "500",
                 "55 Ocean Street",Listing.PaymentFrequencyOptions.MONTHLY, Listing.GenderOptions.MALE,
                 Listing.HousingTypeOptions.PARKPOINT, Listing.IsFurnishedOptions.FURNISHED, Listing.ParkingOption.ON_STR, false);
         Listings.AddListing(listing);
-        listing = new Listing((new Random()).nextLong(), Users.getCurrentUserUID(1234), desc, "500",
+        listing = new Listing(5, Users.getCurrentUserUID(1234), desc, "500",
                 "55 Ocean Street",Listing.PaymentFrequencyOptions.MONTHLY, Listing.GenderOptions.MALE,
                 Listing.HousingTypeOptions.PARKPOINT, Listing.IsFurnishedOptions.FURNISHED, Listing.ParkingOption.ON_STR, true);
         Listings.AddListing(listing);
