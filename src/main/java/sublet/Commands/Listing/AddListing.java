99d03aefd45de4036e56d3d6c820306d5940678c
Ethan Della Posta <ejd3081@rit.edu>
2018-03-21 12:14:18 -0400

    Finished form validation for user and listing, added ListingException class
    Co-Authored-By: gorosin <gorosin@users.noreply.github.com>
    Co-Authored-By: Benjamin Hunt <benjaminhunt@users.noreply.github.com>

--- src/main/java/sublet/Commands/Listing/AddListing.java
+++ src/main/java/sublet/Commands/Listing/AddListing.java
@@ -2,33 +2,58 @@ package sublet.Commands.Listing;
 
 import spark.Request;
 import sublet.Commands.LoggedInCommand;
+import sublet.Exceptions.ListingException;
+import sublet.Exceptions.RegisterException;
 import sublet.controllers.Controller;
 import sublet.models.Listing;
 import sublet.models.Listings;
 import sublet.util.Path;
 
+import java.util.ArrayList;
+
 public class AddListing extends LoggedInCommand{
 
 
     @Override
-    public void ProtectedExecute(Controller controller) {
+    public void ProtectedExecute(Controller controller) throws ListingException{
         Request request = controller.getCurrentRequest();
         if(controller.isLoggedIn()) {
             for (String s:request.queryParams()) {
                 System.out.println(s + " " + request.queryParams(s));
             }
-            Listing L = new Listing(controller.getSessionUser(),
-                    request.queryParams("dis"),
-                    request.queryParams("rent"),
-                    request.queryParams("address"),
-                    Listing.getPaymentValue(request.queryParams("payment")),
-                    Listing.getGenderValue(request.queryParams("gender")),
-                    Listing.getHousing_type(request.queryParams("housing")),
-                    Listing.getIs_furnished(request.queryParams("furn")),
-                    Listing.getParkingValue(request.queryParams("parking")), request.queryParams().contains("utilincluded"));
 
-            Listings.AddListing(L);
-            controller.addRedirect(Path.Web.LISTING + "/" + L.getLID());
+            ArrayList<String> exceptions = isValidFormInputs(request.queryParams("dis"),request.queryParams("address"),
+                    request.queryParams("rent"));
+
+            if(exceptions.size() == 0) {
+                Listing L = new Listing(controller.getSessionUser(),
+                        request.queryParams("dis"),
+                        request.queryParams("rent"),
+                        request.queryParams("address"),
+                        Listing.getPaymentValue(request.queryParams("payment")),
+                        Listing.getGenderValue(request.queryParams("gender")),
+                        Listing.getHousing_type(request.queryParams("housing")),
+                        Listing.getIs_furnished(request.queryParams("furn")),
+                        Listing.getParkingValue(request.queryParams("parking")), request.queryParams().contains("utilincluded"));
+
+                Listings.AddListing(L);
+                controller.addRedirect(Path.Web.LISTING + "/" + L.getLID());
+            }else{
+                StringBuilder exceptionString = new StringBuilder("");
+                for(String re : exceptions){
+                    exceptionString.append(re);
+                }
+                throw new ListingException(exceptionString.toString());
+            }
         }
+    }
+
+    private ArrayList<String> isValidFormInputs(String description, String address, String rent) {
+        ArrayList<String> exceptions = new ArrayList<>();
+
+        if(description.trim().equals("") | address.trim().equals("") | rent.trim().equals(""))
+            exceptions.add("Required fields must be filled.");
+
+        return exceptions;
     }
 }
